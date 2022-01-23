f226e6d65a19fdfb45ae380eb186cac3554e4ccd
james-salafatinos <jamessalafatinos@outlook.com>
2022-01-23 15:52:26 -0600

    initial comit

---
+++ src/preload.js
@@ -0,0 +1,12 @@
+const {contextBridge, ipcRenderer} = require('electron')
+const { currentLoad } = require('systeminformation')
+
+
+contextBridge.exposeInMainWorld("api", {
+    close: () => {
+        ipcRenderer.send("close-app")
+    },
+    getCurrentLoad: () =>{
+        currentLoad()
+    }
+})
